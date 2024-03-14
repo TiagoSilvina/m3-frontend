507dab4ff6c264d74622221dbcffc3ccb706ae90
TiagoSilvina <tiagoafsilvina@gmail.com>
2024-03-14 21:42:59 +0000

    2142

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -5,7 +5,7 @@ import axios from "axios";
 class AuthService {
   constructor() {
     this.api = axios.create({
-      baseURL: import.meta.env.VITE_SERVER_URL || "https://m3-backend.onrender.com"/* "http://localhost:5005" */
+      baseURL: import.meta.env.VITE_SERVER_URL || "https://m3-backend.onrender.com" || "http://localhost:5005" */
     });
 
     this.api.interceptors.request.use((config) => {
