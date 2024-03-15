f9cf2f3b2c93fc2122e62c13a94834f5b70dc026
TiagoSilvina <tiagoafsilvina@gmail.com>
2024-03-15 01:03:44 +0000

    twnav11

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom"
 import { useContext } from "react";
 import { AuthContext } from "../context/auth.context"; 
+import 'tailwindcss/tailwind.css'; // Import Tailwind CSS styles
+import 'daisyui/dist/full.css'; // Import DaisyUI styles
 
 function Navbar() {
 
